4d8c417bc87a5131bee747d399d1c6c167ce2b99
gloriapascuti <108720092+gloriapascuti@users.noreply.github.com>
2023-10-06 16:58:32 +0300

    Added problems from set 2 and 3

---
+++ assignment1/README.md
@@ -0,0 +1,9 @@
+# This is assignment one 
+## First question:
+### Generate the first prime number larger than a given natural number n.
+
+## Second question:
+### Determine the twin prime numbers p1 and p2 immediately larger than the given non-null natural number n.Two prime numbers p and q are called twin if q - p = 2.
+
+## Third question:
+### Generate the largest perfect number smaller than a given natural number n.If such a number does not exist, a message should be displayed. A number is perfect if it is equal to the sum of its divisors, except itself.(e.g. 6 is a perfect number, as 6=1+2+3).
--- assignment1/main.py
+++ assignment1/main.py
@@ -1,13 +1,56 @@
+print("")
+def first_prime_number(number):
+    def is_not_prime(number):
+        flag = False
+        for i in range(2, number):
+            if number % i == 0:
+                flag = True
+        return flag
+
+    while  is_not_prime(number):
+        number = number + 1
+    return number
+
 number = int(input("Enter a number: "))
-def is_not_prime(variable):
-    identical = variable
-    flag = False
-    for i in range(2, identical):
-       if number % i == 0:
-            flag = True
-    return flag
+print("Problem 1: ")
+print(first_prime_number(number))
+print("")
 
-while  is_not_prime(number):
-    number = number + 1
+# Determine the twin prime numbers p1 and p2 immediately larger than the given non-null natural number n.
+# Two prime numbers p and q are called twin if q - p = 2.
 
-print(number)
+def twin_prime(number):
+    first = first_prime_number(number)
+    second = first_prime_number(first+1)
+    while second - first != 2:
+        first = second
+        second = first_prime_number(second+1)
+    if second - first == 2:
+        print((first, second))
+        return True
+print("Problem 2:")
+twin_prime(number)
+print("")
+
+# print("This program is designed to give the use the largest perfect number, smaller than the number given.")
+# number = int(input("So come on and enter a number: "))
+
+def largest_perfect_number(number):
+    def sum_divisors(number):
+        sum = 1
+        for i in range(2, number - 1):
+            if number % i == 0:
+                sum += i
+        return sum
+    number -= 1
+    while sum_divisors(number) != number and number != 0:
+        number -= 1
+    if number == 0 or number == 1:
+        print("Error. Unfortunately such number does not exist.")
+#        number = int(input("Enter a new number: "))
+#        largest_perfect_number(number)
+    else:
+        print(number)
+
+print("Problem 3:")
+largest_perfect_number(number)
